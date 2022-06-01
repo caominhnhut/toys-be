d4ecfef3f524af8ec3b4694acfeeb2e9abd1bf14
Danh Tran <danhtran.it@gmail.com>
2022-06-01 21:35:55 +0700

    reviews product

--- src/main/java/com/momo/toys/be/controller/ProductController.java
+++ src/main/java/com/momo/toys/be/controller/ProductController.java
@@ -9,6 +9,10 @@ import java.util.Set;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
+import com.momo.toys.be.exception.FileStorageException;
+import com.momo.toys.be.factory.mapper.ReviewMapper;
+import com.momo.toys.be.review.Review;
+import com.momo.toys.be.service.ReviewService;
 import org.apache.logging.log4j.util.Strings;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -57,6 +61,9 @@ public class ProductController{
 
     @Autowired
     private CategoryService categoryService;
+
+    @Autowired
+    private ReviewService reviewService;
 
     private Function<com.momo.toys.be.model.Product, Product> buildFromModel = productModel -> {
 
@@ -212,5 +219,46 @@ public class ProductController{
         return ResponseEntity.status(HttpStatus.OK).body(products);
     }
 
+    @PostMapping("/products/{product-id}/reviews")
+    @PreAuthorize("hasRole('ROLE_ADMIN') OR hasRole('ROLE_EMPLOYEE') OR hasRole('ROLE_CUSTOMER')")
+    public ResponseEntity<String> createReview(@PathVariable("product-id") Long productId, @RequestPart("review") Review review, @RequestPart(value = "files", required = false) List<MultipartFile> files) throws NotFoundException, InterruptedException {
+
+        com.momo.toys.be.model.Review reviewModel = ReviewMapper.mapDtoToModel.apply(review);
+
+        if (files != null) {
+            List<Document> images = files.stream().map(DocumentMapper.mapToDocument).collect(Collectors.toList());
+            reviewModel.setImages(images);
+        }
+        reviewModel.setProductId(productId);
+
+        Long reviewId = reviewService.create(reviewModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body("comment added" + reviewId);
+
+    }
+
+    @PutMapping("/products/{product-id}/reviews/{review-id}")
+    @PreAuthorize("hasRole('ROLE_ADMIN') OR hasRole('ROLE_EMPLOYEE') OR hasRole('ROLE_CUSTOMER')")
+    public ResponseEntity<Long> editReview(@PathVariable("product-id") Long productId, @PathVariable("review-id") Long reviewId, @RequestPart("review") Review review, @RequestPart(value = "files", required = true) List<MultipartFile> files) throws NotFoundException, FileStorageException {
+
+        com.momo.toys.be.model.Review reviewModel = ReviewMapper.mapDtoToModel.apply(review);
+
+        List<Document> images = files.stream().map(DocumentMapper.mapToDocument).collect(Collectors.toList());
+        reviewModel.setImages(images);
+        reviewModel.setImages(images);
+        reviewModel.setProductId(productId);
+
+        reviewService.update(reviewModel);
+        return ResponseEntity.status(HttpStatus.OK).body(reviewId);
+
+    }
+
+    @DeleteMapping("/products/{product-id}/reviews/{review-id}")
+    @PreAuthorize("hasRole('ROLE_ADMIN') OR hasRole('ROLE_EMPLOYEE') OR hasRole('ROLE_CUSTOMER')")
+    public ResponseEntity<Boolean> deleteReview(@PathVariable("product-id") Long productId, @PathVariable("review-id") Long reviewId, @RequestParam(name = "is-soft-delete", required = false, defaultValue = "false") Boolean isSoftDelete) throws NotFoundException {
+
+        reviewService.delete(reviewId, isSoftDelete);
+
+        return ResponseEntity.status(HttpStatus.OK).body(true);
+    }
 
 }
--- src/main/java/com/momo/toys/be/entity/UserEntity.java
+++ src/main/java/com/momo/toys/be/entity/UserEntity.java
@@ -1,18 +1,14 @@
 package com.momo.toys.be.entity;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-import java.util.Set;
-
-import javax.persistence.*;
-
-import org.hibernate.annotations.Fetch;
-import org.hibernate.annotations.FetchMode;
 import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 @Entity
 @Table(name = "users")
@@ -28,7 +24,7 @@ public class UserEntity extends BaseEntity implements UserDetails {
     @Column(name = "password")
     private String password;
 
-    @ManyToMany(targetEntity = Authority.class, cascade = {CascadeType.PERSIST, CascadeType.DETACH,CascadeType.MERGE,CascadeType.REFRESH }, fetch = FetchType.EAGER )
+    @ManyToMany(targetEntity = Authority.class, cascade = {CascadeType.PERSIST, CascadeType.DETACH, CascadeType.MERGE, CascadeType.REFRESH}, fetch = FetchType.EAGER)
     @OnDelete(action = OnDeleteAction.CASCADE)
     @JoinTable(name = "authority_role",
             joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
@@ -36,64 +32,64 @@ public class UserEntity extends BaseEntity implements UserDetails {
     private List<Authority> authorities = new ArrayList<>();
 
     @Override
-    public Collection<? extends GrantedAuthority> getAuthorities(){
+    public Collection<? extends GrantedAuthority> getAuthorities() {
         return authorities;
     }
 
     @Override
-    public String getUsername(){
+    public String getUsername() {
         return email;
     }
 
     @Override
-    public boolean isAccountNonExpired(){
+    public boolean isAccountNonExpired() {
         return true;
     }
 
     @Override
-    public boolean isAccountNonLocked(){
+    public boolean isAccountNonLocked() {
         return true;
     }
 
     @Override
-    public boolean isCredentialsNonExpired(){
+    public boolean isCredentialsNonExpired() {
         return true;
     }
 
     @Override
-    public boolean isEnabled(){
+    public boolean isEnabled() {
         return true;
     }
 
-    public Long getId(){
+    public Long getId() {
         return id;
     }
 
-    public void setId(Long id){
+    public void setId(Long id) {
         this.id = id;
     }
 
-    public String getEmail(){
+    public String getEmail() {
         return email;
     }
 
-    public void setEmail(String email){
+    public void setEmail(String email) {
         this.email = email;
     }
 
-    public void setPassword(String password){
+    public void setPassword(String password) {
         this.password = password;
     }
 
-    public String getPassword(){
+    public String getPassword() {
         return password;
     }
 
-    public void setAuthorities(List<Authority> authorities){
+    public void setAuthorities(List<Authority> authorities) {
         this.authorities = authorities;
     }
 
-    public List<Authority> getRoles(){
+    public List<Authority> getRoles() {
         return this.authorities;
     }
 }
